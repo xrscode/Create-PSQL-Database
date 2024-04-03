2d0d0b7a1da166eeae21cafc2d066169cd82eaa5
xrscode <120644276+xrscode@users.noreply.github.com>
2024-04-03 10:00:08 +0100

    .

--- database.js
+++ database.js
@@ -1,46 +1,58 @@
 const fs = require('fs');
 const format = require('pg-format');
 const { Client } = require('pg');
-const { execSync } = require('child_process');
 
-// Seed the database synchronously:
-const command = `./run-all.sh`;
-execSync(command);
-
-const client = new Client({
-    user: 'mac',
-    host: 'localhost',
+// AWS Client Information:
+const client = new Client ({
+    user: 'postgres',
+    host: 'database-5761i.cfk2gikqsjhw.eu-west-2.rds.amazonaws.com',
+    database: 'database5761',
+    password: 'amberdog',
     port: 5432,
-    database: 'temp_staff'
-});
+    // Certificate of Authenticity(?):
+    ssl: {
+        rejectUnauthorized: false}
+})
 
-// Read JSON data asynchronously
-fs.readFile('db/2024-03-05 15-51-00.730403.json', 'utf8', async (err, data) => {
-    if (err) {
-        console.error('Error reading JSON file:', err);
-        return;
-    }
-
-    const jsonData = JSON.parse(data);
-    const queries = [];
-
-    // Generate queries
-    for (const key in jsonData) {
-        const tableNames = Object.keys(jsonData[key][0]).join(', ');
-        const values = jsonData[key].map(x => Object.values(x));
-        const query = format(`INSERT INTO ${key} (${tableNames}) VALUES %L;`, values);
-        queries.push(query);
-    }
-
+async function createTables() {
     try {
-        await client.connect();
-        for (const query of queries) {
-            await client.query(query);
-            console.log('Added!');
+        // Connect to the database.
+        await client.connect(); 
+      
+        // Reads SQL Queries in '02-create-tables.sql'
+        const sqlQueries = fs.readFileSync('db/02-create_tables.sql', 'utf8').split(';')
+
+        // Executes SQL Queries to create tables.
+        for (let i = 0; i < sqlQueries.length; i++){
+            await client.query(sqlQueries[i])
         }
+        
+        // Read from JSON file.
+        const data = fs.readFileSync('db/2024-03-05 15-51-00.730403.json', 'utf8')
+        // Convert into JSON useable object.
+        const jsonData = JSON.parse(data);
+        
+        // Iterate and build queries.   
+        for (const key in jsonData) {
+            jsonData[key][0]
+            // Each key represents individual tables.
+
+            // rows are the names of the rows.
+            const rows = Object.keys(jsonData[key][0]).join(', ');
+           
+            // Generate the values by mapping over data.
+            const values = jsonData[key].map(x => Object.values(x));
+
+            // Create query.
+            const query = format(`INSERT INTO ${key} (${rows}) VALUES %L;`, values);
+
+            await client.query(query);
+            
+        }
+        console.log('Database successfully populated.')
     } catch (error) {
-        console.error('Error executing queries:', error);
+        console.error('Error:', error);
     } finally {
-        await client.end();
+        await client.end(); // Close the client connection.
     }
-});
+}
