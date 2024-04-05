bc2212b6dba240d25f2776ebbfbe7c19733a873a
xrscode <120644276+xrscode@users.noreply.github.com>
2024-04-05 11:33:10 +0100

    Hide Sensitive info and finish setting up Database

--- database.py
+++ database.py
@@ -51,59 +51,6 @@ with open('db/02-create_tables.sql') as file:
     sql = file.read()
 
 
-# # Counterparty function - creates table and populates with data.
-# def create_counterparty_table():
-#     # Establish Connection to Database
-#     create_table = f"""DROP TABLE IF EXISTS counterparty;
-#     CREATE TABLE counterparty (
-#     counterparty_id SERIAL PRIMARY KEY,
-#     counterparty_legal_name VARCHAR,
-#     legal_address_id INTEGER,
-#     commercial_contact VARCHAR,
-#     delivery_contact VARCHAR,
-#     created_at TIMESTAMP WITHOUT TIME ZONE,
-#     last_updated TIMESTAMP WITHOUT TIME ZONE);
-#     """
-#     con.run(create_table)
-#     con.commit()
-#     con.close()
-
-
-# def insert_counterparty():
-#     counterparty = data['counterparty']
-#     # Establish Connection to Database
-#     con = pg8000.connect(
-#         host=credentials['host'],
-#         port=credentials['port'],
-#         database=credentials['database'],
-#         user=credentials['user'],
-#         # password=credentials['password']
-#     )
-
-#     cursor = con.cursor()
-
-#     for item in counterparty:
-#         id = item['counterparty_id']
-#         legal = item['counterparty_legal_name']
-#         address = item['legal_address_id']
-#         commercial = item['commercial_contact']
-#         delivery = item['delivery_contact']
-#         created = item['created_at']
-#         last = item['last_updated']
-
-#         query = f"""
-#         INSERT INTO COUNTERPARTY (counterparty_id, counterparty_legal_name, legal_address_id, commercial_contact, delivery_contact, created_at, last_updated)
-#         VALUES (%s, %s, %s, %s, %s, %s, %s);
-#         """
-#         vals = (id, legal, address, commercial, delivery, created, last)
-#         cursor.execute(query, vals)
-#         con.commit()
-#     cursor.close()
-#     con.close()
-
-# This function will create the database structure.
-
-
 def create_database():
     f"""This function will create a PSQL databse."""
     queries = sql.split('; ')
@@ -114,7 +61,6 @@ def create_database():
         except RuntimeError as e:
             print(e)
     print('Database successfully created.')
-    con.close()
 
 
 def add_data():
@@ -136,9 +82,9 @@ def add_data():
             con.commit()
         print(f'Data added to {table}...moving onto next table now.')
     cursor.close()
-    con.close()
     pass
 
 
-# create_database()
-# add_data()
+create_database()
+add_data()
+con.close()
